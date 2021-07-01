9429b9dee6f2030072b6bce32bb66d6066d63fe2
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-07-01 19:23:24 -0400

    add pics favicon and logo to images folder

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 const Landing = () => {
   return (
     <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+
+        </div>
+      </nav>
 
     </div>
   )
