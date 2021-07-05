2ed2815a81f8aa27a69f3b1b9f160e481091ac65
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-07-05 00:04:51 -0400

    add remaining media queries for contact page

--- client/src/components/contact-section.js
+++ client/src/components/contact-section.js
@@ -14,15 +14,19 @@ const ContactSection = () => {
     <>
       <div className="contact-container">
         <div className="details-container">
-          <h1>CONTACT INFO</h1>
-          <h3>PORT COFFEE COMPANY</h3>
-          <h3>352 COMMERCIAL STREET</h3>
-          <h3>PORTLAND, MAINE</h3>
-          <h3>207-999-9999</h3>
-          <h1>LOCATION HOURS</h1>
-          <h3>SUN CLOSED</h3>
-          <h3>MON - FRI 9am - 4pm</h3>
-          <h3>SAT 10am - 3pm</h3>
+          <div className="info">
+            <h1>CONTACT INFO</h1>
+            <h3>PORT COFFEE COMPANY</h3>
+            <h3>352 COMMERCIAL STREET</h3>
+            <h3>PORTLAND, MAINE</h3>
+            <h3>207-999-9999</h3>
+          </div>
+          <div className="hours">
+            <h1>LOCATION HOURS</h1>
+            <h3>SUN CLOSED</h3>
+            <h3>MON - FRI 9am - 4pm</h3>
+            <h3>SAT 10am - 3pm</h3>
+          </div>
         </div>
         {!isSubmit ? <ContactForm submitForm=
           {submitForm} /> : <FormSuccess />}
